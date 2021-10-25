2c56948c982c39489d2f4d19b082a2accb7dfcaf
radek-drw <rdrweski@gmail.com>
2021-10-25 18:45:43 +0100

    Methods for constellation animation added

--- js/app.js
+++ js/app.js
@@ -6,6 +6,14 @@ class Sky {
       this.ctx = canvas.getContext('2d');
       this.width = window.innerWidth;
       this.height = window.innerHeight;
+      this.lastConstelation = 0;
+      this.nextConstelation = Math.random() * 3000;
+      this.constellation = {
+         stars: [],
+         isClosed: false,
+         width: null,
+      };
+      this.lastUpdate = 0;
    }
 
    initCanvas() {
@@ -37,8 +45,8 @@ class Sky {
 
    updateStars() {
       this.stars.forEach(star => {
-         star.x += star.speed;
-         star.y -= star.speed * ((this.width / 2) - star.x) / 2000;
+         star.x += star.speed * (this.delta / 16);
+         star.y -= star.speed * (this.delta / 16) * ((this.width / 2) - star.x) / 3000;
          star.radius = star.originalRadius * (Math.random() / 3 + 0.8);
 
          if (star.x > this.width + 2 * star.radius) {
@@ -47,10 +55,61 @@ class Sky {
       })
    }
 
+   generateRandomConstellation() {
+      const x = (this.width / 2) + Math.random() * 0.8 * this.width - this.width / 2;
+      const y = (this.height / 2) + Math.random() * 0.8 * this.height - this.height / 2;
+      const radius = (this.height / 2) * Math.random() * 0.5 + 0.5;
+
+      this.constellation = {
+         stars: this.stars.filter(star => {
+            return star.x > x - radius
+               && star.x < x + radius
+               && star.y > y - radius
+               && star.y < y + radius
+         }).slice(0, Math.round(Math.random() * 7 + 3)),
+         isClosed: Math.random() > 0.5,
+         width: 5,
+      }
+   }
+
+   updateConstellation() {
+      if (this.constellation.width > 0) {
+         this.constellation.width -= 0.04 * (this.delta / 16);
+      } else {
+         this.constellation.width = 0;
+      }
+   }
+
+   drawConstellation() {
+      const { stars, isClosed, width } = this.constellation;
+      const starsCount = stars.length;
+
+      if (starsCount > 2) {
+         const firstStar = stars[0];
+
+         this.ctx.beginPath();
+         this.ctx.moveTo(firstStar.x, firstStar.y);
+         this.ctx.lineTo(stars[1].x, stars[1].y);
+
+         for (let i = 1; i < starsCount - 1; i++) {
+            const nextStar = stars[i + 1];
+            this.ctx.lineTo(nextStar.x, nextStar.y);
+         }
+
+         if (isClosed) {
+            this.ctx.lineTo(firstStar.x, firstStar.y);
+         }
+
+         this.ctx.strokeStyle = '#f7eada';
+         this.ctx.lineWidth = width;
+         this.ctx.stroke();
+      }
+   }
+
    drawOverlayer() {
       let gradient = this.ctx.createRadialGradient(this.width / 2, this.height / 2, 250, this.width / 2, this.height / 2, this.width / 2);
       gradient.addColorStop(0, 'rgba(0, 0, 0, 0)');
-      gradient.addColorStop(1, 'rgba(0, 0, 0, 0.77)');
+      gradient.addColorStop(1, 'rgba(0, 0, 0, 0.75)');
 
       this.ctx.fillStyle = gradient;
       this.ctx.fillRect(0, 0, this.width, this.height);
@@ -88,21 +147,35 @@ class Sky {
       this.ctx.restore();
    }
 
-   draw() {
+   draw(now) {
+      this.delta = now - this.lastUpdate;
+
       this.clearCanvas();
 
       this.drawStars();
-      // this.updateStars();
+      this.updateStars();
+
+      this.drawConstellation();
+      this.updateConstellation();
 
       this.drawOverlayer();
 
-      window.requestAnimationFrame(() => this.draw());
+      if (now - this.lastConstelation > this.nextConstelation) {
+         this.lastConstelation = now;
+         this.nextConstelation = Math.random() * 1000 + 2000;
+         this.generateRandomConstellation();
+      }
+
+      this.lastUpdate = now;
+
+      window.requestAnimationFrame((now) => this.draw(now));
    }
 
    run() {
       this.initCanvas();
       this.generateStars(400);
-      this.draw();
+      this.generateRandomConstellation();
+      this.draw(0);
    }
 }
 
